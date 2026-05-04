97d6742d2047fd3cf2734cd0ea81ebdd937eb2e0
Im <imranfitri06@gmail.com>
2026-05-04 23:35:15 +0800

    added tailwindcss

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -3,9 +3,12 @@ import { useNavigate } from "react-router-dom";
 export default function HomePage() {
     const navigate = useNavigate();
     return (
-        <div>
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
             <h1>Math Battle</h1>
             <button onClick={() => navigate("/lobby")}>Enter lobby</button>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
 }
