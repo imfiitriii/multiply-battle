d25f41166587cdb3475301c3dab96a909f80755c
Im <imranfitri06@gmail.com>
2026-05-07 22:41:40 +0800

    added registerpage

--- src/features/auth/register/RegisterPage.jsx
+++ src/features/auth/register/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import Background from "../../../components/Background";
-// import RegisterForm from "../../../components/RegisterForm";
+import RegisterForm from "../../../components/RegisterForm";
 
 export default function RegisterPage() {
     return (
